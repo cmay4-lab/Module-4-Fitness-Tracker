819ac514613a8906b83c366c84fc2056e4c2c8d3
cmay4-lab <cmay4@bucs.fsw.edu>
2024-10-11 20:18:40 -0400

    Update before big change, a lot of progress, nearly done. Next, I will do some commenting and cleaning up the comments I already have. I will also be sure convention is being followed.

--- FitnessAppTester.java
+++ FitnessAppTester.java
@@ -1,58 +1,72 @@
 public class FitnessAppTester {
+    private static int currentGoalSteps;
 
     // Updating
-    private static void addSteps(int steps) {
-        FitnessTracker FitnessTracker = new FitnessTracker();
+    private static void addSteps(int steps, FitnessTracker fitnessTracker) {
+        // Declaration and definition
+        int pastStepCount = fitnessTracker.getDailySteps();
+        int newStepCount = pastStepCount + steps;
 
-        int currentStepCount = FitnessTracker.getDailySteps();
-        int newStepCount = currentStepCount + steps;
+        // Update
+        fitnessTracker.setDailySteps(newStepCount);
 
-        System.out.print("\n\nTest 1: adding steps to zero...\n");
-        System.out.print("The FitnessTracker object has a property: dailySteps. From which, the number of steps is currently... " + currentStepCount + "\n");
-        System.out.print("However, the added steps updates the step count to... " + newStepCount);
-
-        FitnessTracker = new FitnessTracker("Larry123", 2279, 0.974598, 40.8, 85);
-
-        currentStepCount = FitnessTracker.getDailySteps();
-        newStepCount = currentStepCount + steps;
-
-        System.out.print("\n\nTest 2: adding steps to past step count...\n");
-        System.out.print("The FitnessTracker object has a property: dailySteps. From which, the number of steps is currently... " + currentStepCount + "\n");
-        System.out.print("However, the added steps updates the step count to... " + newStepCount);
-    }
-
-    private static void calculateDistance() {
-        FitnessTracker FitnessTracker = new FitnessTracker();
-
-        double currentDistance = FitnessTracker.getDistanceWalked();
-        double newDistance = FitnessTracker.getDistanceWalked() + 
-
-        System.out.print("\n\nTest 3: adding the same steps but in kilometers to the distance walked...\n");
-        System.out.print("The FitnessTracker object has a property: distanceWalked. From which, the distance walked is currently... " + currentDistance + "\n");
-        System.out.print("However, the added steps updates the distance walked to... " + newStepCount);
-
-        FitnessTracker = new FitnessTracker("Larry123", 2279, 0.974598, 40.8, 85);
-
-        currentStepCount = FitnessTracker.getDailySteps();
-        newStepCount = currentStepCount + steps;
-
+        // Displaying test results in immersive way
         System.out.print("\n\nTest 1: adding steps to past step count...\n");
-        System.out.print("The FitnessTracker object has a property: dailySteps. From which, the number of steps is currently... " + currentStepCount + "\n");
-        System.out.print("However, the added steps updates the step count to... " + newStepCount);
-
+        System.out.print("The FitnessTracker object has a property: dailySteps. From which, the user had... " + pastStepCount + "\n");
+        System.out.print("However, after an update, the user's new step count is... " + newStepCount);
     }
 
-    private static void calculateCalories() {
+    private static void calculateDistance(FitnessTracker fitnessTracker) {
+        // Declaration and definition
+        double pastDistance = fitnessTracker.getDistanceWalked();
+        double newDistance = pastDistance + fitnessTracker.getDailySteps()*FitnessTracker.kilometersPerStep;
 
+        // Update
+        fitnessTracker.setDistanceWalked(newDistance);
+
+        // Displaying test results in immersive way
+        System.out.print("Test 2: calcuating the distance walked in kilometers from the number of steps walked...\n");
+        System.out.print("The FitnessTracker object has a property: distanceWalked. From which, the user had... " + pastDistance + "km" + "\n");
+        System.out.print("However, after an update, the user's new 'distance walked' is... " + newDistance + "km");
     }
 
-    private static void updateHeartRate(int newHeartRate) {
+    private static void calculateCalories(FitnessTracker fitnessTracker) {
+        // Declaration and definition
+        double pastCalorieBurned = fitnessTracker.getCaloriesBurned();
+        double newCaloriesBurned = pastCalorieBurned + fitnessTracker.getDailySteps()*FitnessTracker.caloriesPerStep;
 
+        // Update
+        fitnessTracker.setCaloriesBurned(newCaloriesBurned);
+        // Displaying test results in immersive way
+        System.out.print("Test 3: calculating the calories burned from the number of steps walked...\n");
+        System.out.print("The FitnessTracker object has a property: caloriesBurned. From which, the user had... " + pastCalorieBurned + "Calories." + "\n");
+        System.out.print("However, after an update, the user's new 'calories burned' is... " + newCaloriesBurned + "Calories.");
+    }
+
+    private static void updateHeartRate(int newHeartRate, FitnessTracker fitnessTracker) {
+        // Update
+        fitnessTracker.setHeartRate(newHeartRate);
+        // Displaying test results in immersive way
+        System.out.print("Test 4: updating the heart rate...\n");
+        System.out.print("The FitnessTracker object has a property: heartRate.\n");
+        System.out.print("However, after an update, the user's new 'calories burned' is... " + newHeartRate + "Beats Per Minute.");
     }
 
     // Resetting
-    private static void resetDailyStats() {
+    private static void resetDailyStats(FitnessTracker fitnessTracker) {
+        fitnessTracker.setDailySteps(0);
+        fitnessTracker.setDistanceWalked(0);
+        fitnessTracker.setCaloriesBurned(0);
 
+        int newDailySteps = fitnessTracker.getDailySteps();
+        double newDistanceWalked = fitnessTracker.getDistanceWalked();
+        double newCaloriesBurned = fitnessTracker.getCaloriesBurned();
+
+        // Displaying test results in immersive way
+        System.out.print("Resetting daily stats:\n");
+        System.out.print("Daily steps is now equal to... " + newDailySteps + "\n");
+        System.out.print("Total distance walked is now equal to... " + newDistanceWalked + "\n");
+        System.out.print("Total calories burned is now equal to... " + newCaloriesBurned);
     }
 
     // Retrieving
@@ -60,28 +74,99 @@ public class FitnessAppTester {
 
     // Other
     private static void setGoalSteps(int goalSteps) {
-
+        // Update
+        FitnessAppTester.currentGoalSteps = goalSteps;
+        // Displaying test results in immersive way
+        System.out.print("Current goal has been updated!\n");
+        System.out.print("Your new current goal is... " + currentGoalSteps);
     }
 
-    private static void checkGoal() {
-
+    private static void checkGoal(FitnessTracker fitnessTracker) {
+        int currentStepGoal = FitnessAppTester.currentGoalSteps;
+        int currentStepTotal = fitnessTracker.getDailySteps();
+        
+        // Update & Displaying test results in immersive way
+        if (currentStepGoal > currentStepTotal) {
+            System.out.print("You have not reached your goal yet...");
+        } else {
+            System.out.print("You have reached your step goal! Great Job!");
+        }
     }
 
     public static void main(String[] args) {
-        // Test constants  
-        System.out.println(FitnessTracker.kilometersPerStep);
-        System.out.println(FitnessTracker.caloriesPerStep);
-        
-        FitnessTracker fitnessTracker = new FitnessTracker();
+        System.out.print("\n\n");
 
-        // Larry is a new user, and wants to be more healthy. Their stats are currently blank but he is going to fix that.
+        // Test constants:
+        System.out.print("Text Constants: ")
+        System.out.print(FitnessTracker.kilometersPerStep + "\n");
+        System.out.print(FitnessTracker.caloriesPerStep + "\n");
+
         
-        // Larry first puts in a username:
+        // Imaginary examples:
+
+        // Larry - Default (No Parameters)
+        FitnessTracker fitnessTracker = new FitnessTracker();
+        // Declare goalSteps
+
+        // Larry is a new user, and wants to be more healthy. His stats start out blank but he is going to fix that.
         fitnessTracker.setName("Larry123");
-        // Larry then goes on a jog with his fitness tracker on, which will update all his stats:
-        addSteps(300);
-        calculateDistance();
-        calculateCalories();
+    
+        // Larry goes jogging after setting a goal for himself. His tracker updates overtime.
+        setGoalSteps(5000);
+        System.out.print("\n\n");
+        
+        addSteps(3102, fitnessTracker);
+        System.out.print("\n\n");
+        
+        calculateDistance(fitnessTracker);
+        System.out.print("\n\n");
+        
+        calculateCalories(fitnessTracker);
+        System.out.print("\n\n");
+        
+        updateHeartRate(110, fitnessTracker);
+        System.out.print("\n\n");
+
+        // Near the end of the day Larry checks to see his stats and whether he reached his goal
+        fitnessTracker.toString();
+        System.out.print("\n\n");
+
+        checkGoal(fitnessTracker);
+        System.out.print("\n\n");
+
+        // He then goes asleep, when midnight comes by his daily stats update:
+        resetDailyStats(fitnessTracker);
+        System.out.print("\n\n");
+
+        // Then attempts to break through his goal again today
+        addSteps(5219, fitnessTracker);
+        System.out.print("\n\n");
+        
+        calculateDistance(fitnessTracker);
+        System.out.print("\n\n");
+        
+        calculateCalories(fitnessTracker);
+        System.out.print("\n\n");
+        
+        updateHeartRate(96, fitnessTracker);
+        System.out.print("\n\n");
+
+        // He checks to see his stats, and he was amaze with his achievements because it did not feel that difficult that time.
+        fitnessTracker.toString();
+        System.out.print("\n\n");
+
+        checkGoal(fitnessTracker);
+        System.out.print("\n\n");
+
+        // He goes home to rest: "this was fun!"
+        resetDailyStats(fitnessTracker);
+        System.out.print("\n\n");
+
+
+        // Mary
+        //FitnessTracker marryFitnessTracker = new FitnessTracker("Merry123", 2279, 0.974598, 40.8, 85);
+
+        
 
 
         
