5c20e19119ca0e0e03143018d6658d21fa4c0f2c
cmay4-lab <cmay4@bucs.fsw.edu>
2024-10-11 21:58:15 -0400

    I STILL NEED TO DO MORRRE

--- FitnessAppTester.java
+++ FitnessAppTester.java
@@ -1,59 +1,75 @@
+/**
+ * 
+ * FitnessAppTester --- Program to test and print values from the FitnessTracker Class
+ * @author              Chase May
+ */
 public class FitnessAppTester {
-    private static int currentGoalSteps;
 
-    // Updating
+    private static int currentGoalSteps;    // Stores the current daily step goal that the user makes.
+
+    /**
+     * 
+     * addSteps --- Adds steps to the daily step field.
+     * @param       steps
+     * @param       fitnessTracker
+     * @return      No return value.
+     */
     private static void addSteps(int steps, FitnessTracker fitnessTracker) {
-        // Declaration and definition
-        int pastStepCount = fitnessTracker.getDailySteps();
-        int newStepCount = pastStepCount + steps;
 
-        // Update
-        fitnessTracker.setDailySteps(newStepCount);
+        int pastStepCount = fitnessTracker.getDailySteps();    // Stores the existing total of daily steps into pastStepCount.
+        int newStepCount = pastStepCount + steps;              // Sums up the pastStepCount with the steps and store into newStepCount.
 
-        // Displaying test results in immersive way
+        fitnessTracker.setDailySteps(newStepCount);            // Sets the dailySteps field of fitnessTracker to equal newStepCount. 
+
         System.out.print("\n\nTest 1: adding steps to past step count...\n");
         System.out.print("The FitnessTracker object has a property: dailySteps. From which, the user had... " + pastStepCount + "\n");
         System.out.print("However, after an update, the user's new step count is... " + newStepCount);
     }
 
+    /**
+     * 
+     * calculateDistance --- Calculates the updated distance walked to set it into the distanceWalked field.  
+     * @param                fitnessTracker
+     * @return               No return value.
+     */
     private static void calculateDistance(FitnessTracker fitnessTracker) {
-        // Declaration and definition
+
         double pastDistance = fitnessTracker.getDistanceWalked();
         double newDistance = pastDistance + fitnessTracker.getDailySteps()*FitnessTracker.kilometersPerStep;
 
-        // Update
         fitnessTracker.setDistanceWalked(newDistance);
 
-        // Displaying test results in immersive way
         System.out.print("Test 2: calcuating the distance walked in kilometers from the number of steps walked...\n");
         System.out.print("The FitnessTracker object has a property: distanceWalked. From which, the user had... " + pastDistance + "km" + "\n");
         System.out.print("However, after an update, the user's new 'distance walked' is... " + newDistance + "km");
+        
     }
 
     private static void calculateCalories(FitnessTracker fitnessTracker) {
-        // Declaration and definition
+
         double pastCalorieBurned = fitnessTracker.getCaloriesBurned();
         double newCaloriesBurned = pastCalorieBurned + fitnessTracker.getDailySteps()*FitnessTracker.caloriesPerStep;
 
-        // Update
         fitnessTracker.setCaloriesBurned(newCaloriesBurned);
-        // Displaying test results in immersive way
+
         System.out.print("Test 3: calculating the calories burned from the number of steps walked...\n");
         System.out.print("The FitnessTracker object has a property: caloriesBurned. From which, the user had... " + pastCalorieBurned + "Calories." + "\n");
         System.out.print("However, after an update, the user's new 'calories burned' is... " + newCaloriesBurned + "Calories.");
+
     }
 
     private static void updateHeartRate(int newHeartRate, FitnessTracker fitnessTracker) {
-        // Update
+
         fitnessTracker.setHeartRate(newHeartRate);
-        // Displaying test results in immersive way
+
         System.out.print("Test 4: updating the heart rate...\n");
         System.out.print("The FitnessTracker object has a property: heartRate.\n");
         System.out.print("However, after an update, the user's new 'calories burned' is... " + newHeartRate + "Beats Per Minute.");
+
     }
 
-    // Resetting
     private static void resetDailyStats(FitnessTracker fitnessTracker) {
+
         fitnessTracker.setDailySteps(0);
         fitnessTracker.setDistanceWalked(0);
         fitnessTracker.setCaloriesBurned(0);
@@ -62,56 +78,56 @@ public class FitnessAppTester {
         double newDistanceWalked = fitnessTracker.getDistanceWalked();
         double newCaloriesBurned = fitnessTracker.getCaloriesBurned();
 
-        // Displaying test results in immersive way
+     
         System.out.print("Resetting daily stats:\n");
         System.out.print("Daily steps is now equal to... " + newDailySteps + "\n");
         System.out.print("Total distance walked is now equal to... " + newDistanceWalked + "\n");
         System.out.print("Total calories burned is now equal to... " + newCaloriesBurned);
+
     }
 
-    // Retrieving
-    // toString method already exists.
-
-    // Other
     private static void setGoalSteps(int goalSteps) {
-        // Update
+     
         FitnessAppTester.currentGoalSteps = goalSteps;
-        // Displaying test results in immersive way
+
         System.out.print("Current goal has been updated!\n");
         System.out.print("Your new current goal is... " + currentGoalSteps);
+
     }
 
     private static void checkGoal(FitnessTracker fitnessTracker) {
+
         int currentStepGoal = FitnessAppTester.currentGoalSteps;
         int currentStepTotal = fitnessTracker.getDailySteps();
         
-        // Update & Displaying test results in immersive way
         if (currentStepGoal > currentStepTotal) {
             System.out.print("You have not reached your goal yet...");
         } else {
             System.out.print("You have reached your step goal! Great Job!");
         }
+
     }
 
     public static void main(String[] args) {
+
         System.out.print("\n\n");
 
-        // Test constants:
-        System.out.print("Text Constants: ")
+        //
+        System.out.print("Text Constants: ");
         System.out.print(FitnessTracker.kilometersPerStep + "\n");
         System.out.print(FitnessTracker.caloriesPerStep + "\n");
 
         
-        // Imaginary examples:
+        //
 
-        // Larry - Default (No Parameters)
+        //
         FitnessTracker fitnessTracker = new FitnessTracker();
-        // Declare goalSteps
+        //
 
-        // Larry is a new user, and wants to be more healthy. His stats start out blank but he is going to fix that.
+        //
         fitnessTracker.setName("Larry123");
     
-        // Larry goes jogging after setting a goal for himself. His tracker updates overtime.
+        //
         setGoalSteps(5000);
         System.out.print("\n\n");
         
@@ -127,18 +143,18 @@ public class FitnessAppTester {
         updateHeartRate(110, fitnessTracker);
         System.out.print("\n\n");
 
-        // Near the end of the day Larry checks to see his stats and whether he reached his goal
+        //
         fitnessTracker.toString();
         System.out.print("\n\n");
 
         checkGoal(fitnessTracker);
         System.out.print("\n\n");
 
-        // He then goes asleep, when midnight comes by his daily stats update:
+        //
         resetDailyStats(fitnessTracker);
         System.out.print("\n\n");
 
-        // Then attempts to break through his goal again today
+        //
         addSteps(5219, fitnessTracker);
         System.out.print("\n\n");
         
@@ -151,26 +167,15 @@ public class FitnessAppTester {
         updateHeartRate(96, fitnessTracker);
         System.out.print("\n\n");
 
-        // He checks to see his stats, and he was amaze with his achievements because it did not feel that difficult that time.
+        //
         fitnessTracker.toString();
         System.out.print("\n\n");
 
         checkGoal(fitnessTracker);
         System.out.print("\n\n");
 
-        // He goes home to rest: "this was fun!"
+        //
         resetDailyStats(fitnessTracker);
         System.out.print("\n\n");
-
-
-        // Mary
-        //FitnessTracker marryFitnessTracker = new FitnessTracker("Merry123", 2279, 0.974598, 40.8, 85);
-
-        
-
-
-        
-
- 
     }
 }
--- FitnessTracker.java
+++ FitnessTracker.java
@@ -1,65 +1,161 @@
+/**
+ * 
+ * FitnessTracker --- Class to organize the necessary properties and methods to be used in FitnessAppTester.
+ * @author            Chase May
+ */
 public class FitnessTracker {
-    // public properties (constants)
-    public static final double kilometersPerStep = 0.000762;
-    public static final double caloriesPerStep = 0.04;
-    // Private properties
-    private String name;
-    private int dailySteps;
-    private double distanceWalked;
-    private double caloriesBurned;
-    private int heartRate;
+    public static final double kilometersPerStep = 0.000762;    // Converts steps to kilometers
+    public static final double caloriesPerStep = 0.04;          // Converts steps to calories
     
+    private String name;                // Stores username.
+    private int dailySteps;             // Stores the number of steps taken.
+    private double distanceWalked;      // Stores distance walked.
+    private double caloriesBurned;      // Stores calories burned.
+    private int heartRate;              // Stores heart rate.
 
-    // userName
+    /**
+     * 
+     * getName --- Gets username. 
+     * @return     username
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * 
+     * setName --- Changes username.
+     * @param      userName
+     * @return     No return value.
+     */
     public void setName(String userName) {
+
         name = userName;
-    }
-    // dailySteps
-    public int getDailySteps() {
-        return dailySteps;
-    }
-    public void setDailySteps(int userDailySteps) {
-        dailySteps = userDailySteps;
-    }
-    // distanceWalked
-    public double getDistanceWalked() {
-        return distanceWalked;
-    }
-    public void setDistanceWalked(double userDistanceWalked) {
-        distanceWalked = userDistanceWalked;
-    }
-    // caloriesBurned
-    public double getCaloriesBurned() {
-        return caloriesBurned;
-    }
-    public void setCaloriesBurned(double userCaloriesBurned) {
-        caloriesBurned = userCaloriesBurned;
-    }
-    // heartRate
-    public int getHeartRate() {
-        return heartRate;
-    }
-    public void setHeartRate(int userHeartRate) {
-        heartRate = userHeartRate;
+
     }
 
-    // Default Constructor
+    /**
+     * 
+     * getDailySteps --- Gets the current daily step total. 
+     * @return           dailySteps
+     */
+    public int getDailySteps() {
+
+        return dailySteps;
+
+    }
+
+    /**
+     * 
+     * setDailySteps --- Updates the daily steps total.
+     * @param            userDailySteps
+     * @return           No return value.
+     */
+    public void setDailySteps(int userDailySteps) {
+
+        dailySteps = userDailySteps;
+
+    }
+
+    /**
+     * 
+     * getDistanceWalked --- Gets the current distance walked today.
+     * @return               distanceWalked
+     */
+    public double getDistanceWalked() {
+
+        return distanceWalked;
+
+    }
+
+    /**
+     * 
+     * setDistanceWalked --- Updates the distance walked today. 
+     * @param                userDistanceWalked
+     * @return               No return value.
+     */
+    public void setDistanceWalked(double userDistanceWalked) {
+
+        distanceWalked = userDistanceWalked;
+
+    }
+    
+    /**
+     * 
+     * getCaloriesBurned --- Gets the current calories burned today.
+     * @return               caloriesBurned
+     */
+    public double getCaloriesBurned() {
+
+        return caloriesBurned;
+
+    }
+
+    /**
+     * 
+     * setCaloriesBurned --- Updates the calories burned today.
+     * @param                userCaloriesBurned
+     * @return               No return value.
+     */
+    public void setCaloriesBurned(double userCaloriesBurned) {
+
+        caloriesBurned = userCaloriesBurned;
+
+    }
+
+    /**
+     * 
+     * getHeartRate --- Gets the user's current heart rate.
+     * @return          heartRate
+     */
+    public int getHeartRate() {
+
+        return heartRate;
+
+    }
+
+    /**
+     * 
+     * setHeartRate --- Updates the user's current heart rate.
+     * @param           userHeartRate
+     * @return          No return value.
+     */
+    public void setHeartRate(int userHeartRate) {
+
+        heartRate = userHeartRate;
+
+    }
+
+    /**
+     * 
+     * FitnessTracker (Default Constructor) --- Constructs a FitnessTracker object with default fields 
+     */
     public FitnessTracker() {
+
         name = "";
         dailySteps = 0;
         distanceWalked = 0;
         caloriesBurned = 0;
         heartRate = 0;
+
     }
-    // Parameter Constructor (when properties are known)
+
+    /**
+     * 
+     * FitnessTracker (Parameterized Constructor) --- Constructs a FitnessTracker object with the given parameters as field initalizers.
+     * @param userName
+     * @param userDailySteps
+     * @param userDistanceWalked
+     * @param userCaloriesBurned
+     * @param userHeartRate
+     */
     public FitnessTracker(String userName, int userDailySteps, double userDistanceWalked, double userCaloriesBurned, int userHeartRate) {
+
         name = userName;
         dailySteps = userDailySteps;
         distanceWalked = userDistanceWalked;
         caloriesBurned = userCaloriesBurned;
         heartRate = userHeartRate;
+        
     }
 }
